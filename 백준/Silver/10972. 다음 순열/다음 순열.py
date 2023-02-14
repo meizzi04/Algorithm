39e8a8f99bca1f2a20a041e880dedd6fae1f804d
김미령 <86556226+meizzi04@users.noreply.github.com>
2023-02-14 14:53:06 +0900

    [Silver III] Title: 다음 순열, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.py
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -1,13 +1,14 @@
-n = int(input())
-data = list(map(int, input().split()))
+N = int(input())
+num = list(map(int, input().split()))
 
-for i in range(n-1, 0, -1): # 맨 뒤 값부터 시작
-    if data[i-1] < data[i]:
-        for j in range(n-1, 0, -1): # 다시 맨 뒤 값부터 큰 값찾기
-            if data[i-1] < data[j]:
-                data[i-1], data[j] = data[j], data[i-1] # 둘 값을 swap
-                data = data[:i] + sorted(data[i:])
-                for i in data:
-                    print(i, end=' ')
+for i in range(N-1, 0, -1) : # 뒤에서부터 비교
+    if num[i] > num[i-1] : # 뒤에 있는 값이 크면 내림차순이 아니라는 의미
+        for j in range(N-1, 0, -1) : # 다시 뒤에서부터 비교
+            if num[i-1] < num[j] :
+                num[i-1], num[j] = num[j], num[i-1]
+                num = num[:i] + sorted(num[i:])
+
+                for k in num :
+                    print(k, end = ' ')
                 exit()
 print(-1)
