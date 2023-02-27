fca1521539c4f5b3d13fc2b0a4b1e815a73ebab4
김미령 <86556226+meizzi04@users.noreply.github.com>
2023-02-27 20:13:43 +0900

    [Silver V] Title: 나이순 정렬, Time: 4320 ms, Memory: 46276 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,12 @@
+N = int(input())
+member = []
+
+for i in range(N):
+    age, name = map(str, input().split())
+    age = int(age)
+    member.append((age, name))
+
+member.sort(key=lambda x: x[0])
+
+for i in member:
+    print(i[0], i[1])
