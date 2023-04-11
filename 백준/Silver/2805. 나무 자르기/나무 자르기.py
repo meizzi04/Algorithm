62747957d97f0266b4337bd56cee5c24437fe4c5
김미령 <86556226+meizzi04@users.noreply.github.com>
2023-04-11 20:04:09 +0900

    [Silver II] Title: 나무 자르기, Time: 4840 ms, Memory: 150336 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+tree = list(map(int, input().split()))
+start, end = 1, max(tree)
+
+while start <= end:
+    mid = (start+end) // 2
+    cut = 0
+
+    for i in tree:
+        if i >= mid:
+            cut += i - mid
+
+    if cut >= M:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
